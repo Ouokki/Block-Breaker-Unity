d267ed8fb99a2bc8e160624431ad37ca43eda931
Mohamed ouokki <mohamed.ouokki@um5s.net.ma>
2020-05-11 18:46:42 +0200

    jenkins test

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -7,5 +7,6 @@ class MyEditorScript
         string[] defaultScene = { "Assets/Scenes/Levels.unity" };
         BuildPipeline.BuildPlayer(defaultScene, "./builds/game.x86_64",
             BuildTarget.StandaloneLinux64, BuildOptions.None);
+              Console.Write("first test ------------------");
         }
 }
